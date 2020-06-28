dd7d92951501f175e30cf550d15bdb3a6f970d7a
mattfeng <mattfeng@mit.edu>
2020-06-28 11:22:09 -0400

    update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,20 @@
 const path = require("path")
 
 module.exports = {
-
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
 }
