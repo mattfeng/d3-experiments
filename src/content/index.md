d3c0615cbe7df53a0adf2bdf6e610636b826c90e
mattfeng <mattfeng@mit.edu>
2020-06-28 19:41:23 -0400

    update info

--- src/content/index.md
+++ src/content/index.md
@@ -1,3 +1,13 @@
 # Hospitals in the United States
 
-<svg id="us-hospitals" width="800" height="520" viewBox="0 0 800 520"></svg>
+<svg id="us-hospitals" width="760" height="500" viewBox="0 0 760 500"></svg>
+
+This data is taken from the [Centers for Medicare and Medicaid Services](https://www.cms.gov/), following the methodology outlined [here](https://www.medicare.gov/hospitalcompare/About/Hospital-overall-ratings.html). In summary, the rankings are based on 57 quality measures that fall into one of seven categories:
+
+1. Effectiveness of care
+2. Efficient use of medical imaging
+3. Mortality
+4. Patient experience
+5. Readmissions
+6. Safety of care
+7. Timeliness of care
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import content from "./content/index.md"
 import data from "./data/counties-10m.json"
+import hospitalRanking from "./data/fips-hospital-data.json"
+
 import {
   select,
   geoPath,
-  geoAlbersUsa
+  geoAlbersUsa,
+  scaleSequential,
+  interpolatePuBu,
+  zoom,
+  event
 } from "d3"
 import { feature } from "topojson"
 import "./styles/main.scss"
@@ -28,8 +34,14 @@ const marginG = svg.append('g')
   .attr("height", innerHeight)
   .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
+const zoomG = marginG.append('g')
+svg.call(zoom().on("zoom", () => {
+  zoomG.attr("transform", event.transform)
+}))
+
 // get the data
 const counties = feature(data, data.objects.counties)
+const states = feature(data, data.objects.states)
 
 const projection = geoAlbersUsa()
   .fitSize([innerWidth, innerHeight], counties)
@@ -37,12 +49,49 @@ const projection = geoAlbersUsa()
 
 const path = geoPath().projection(projection)
 
-marginG.selectAll(".region")
+// set color scale
+const colorScale = scaleSequential(interpolatePuBu)
+  .domain([1, 5])
+
+const fipsInfo = (d) => {
+  const hospitals = hospitalRanking[d.id]
+
+  let info = ""
+  let avg = "n/a"
+
+  if (hospitals) {
+    let sum = 0
+    hospitals.forEach(([name, ranking]) => {
+      sum += ranking
+      info += `${name}: ${ranking}\n`
+    })
+    avg = sum / hospitals.length
+  }
+
+  info = `${d.properties.name} County (avg: ${avg})\n` + info
+
+  return [avg === "n/a" ? "#ccc" : colorScale(avg), info]
+}
+
+
+zoomG.selectAll(".county")
   .data(counties.features)
   .enter().append("path")
     .attr("d", path)
-    .attr("class", "region")
-    .attr("fill", "#e9e9e9")
-    .attr("stroke", "none")
+    .attr("class", "county")
+    .attr("fill", d => fipsInfo(d)[0])
+  .append("title")
+    .text(d => fipsInfo(d)[1])
+
+zoomG.selectAll(".state")
+  .data(states.features)
+  .enter().append("path")
+    .attr("d", path)
+    .attr("class", "state")
+    .attr("fill", "none")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", "0.5px")
+
+
 
 console.log(data)
